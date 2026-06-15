9de1d8519d5cfa28352873c71e0266c604dec345
Ratko Ćosić <ratko_cosic@hotmail.com>
2026-06-15 18:33:22 +0200

    scaffold

---
+++ NoBeard.Learn.AspNet.MvcApp/Controllers/AccountController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NoBeard.Learn.AspNet.MvcApp.Models;
+
+namespace NoBeard.Learn.AspNet.MvcApp.Controllers;
+
+public class AccountController : Controller
+{
+    // GET: Account
+    public ActionResult Index()
+    {
+        var account = new Account        
+        {
+            Id = 1,
+            Name = "Tekući račun",
+            Total = 200
+        };
+        
+        var accounts = new List<Account> { account };
+
+        return View(accounts);
+    }
+
+    // GET: Account/Details/5
+    public ActionResult Details(int id)
+    {
+        var account = new Account
+        {
+            Id = 5,
+            Name = "Tekući račun",
+            Total = 200
+        };
+
+        return View(account);
+    }
+
+    // GET: AccountController/Create
+    public ActionResult Create()
+    {
+        return View(new Account());
+    }
+
+    // POST: AccountController/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Create(IFormCollection collection)
+    {
+        try
+        {
+            return RedirectToAction(nameof(Index));
+        }
+        catch
+        {
+            return View();
+        }
+    }
+
+    // GET: AccountController/Edit/5
+    public ActionResult Edit(int id)
+    {
+        return View();
+    }
+
+    // POST: AccountController/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Edit(int id, IFormCollection collection)
+    {
+        try
+        {
+            return RedirectToAction(nameof(Index));
+        }
+        catch
+        {
+            return View();
+        }
+    }
+
+    // GET: AccountController/Delete/5
+    public ActionResult Delete(int id)
+    {
+        return View();
+    }
+
+    // POST: AccountController/Delete/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Delete(int id, IFormCollection collection)
+    {
+        try
+        {
+            return RedirectToAction(nameof(Index));
+        }
+        catch
+        {
+            return View();
+        }
+    }
+}
---
+++ NoBeard.Learn.AspNet.MvcApp/Models/Account.cs
@@ -0,0 +1,21 @@
+﻿namespace NoBeard.Learn.AspNet.MvcApp.Models;
+
+public class Account
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public decimal Total { get; set; }
+
+    public List<AccountTransaction> Transactions { get; set; } = [];
+}
+
+public class AccountTransaction
+{
+    public Guid Id { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public string? Note { get; set; }
+}
