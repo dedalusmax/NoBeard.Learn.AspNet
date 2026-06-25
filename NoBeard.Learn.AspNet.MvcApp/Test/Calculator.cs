beea2958fce0abc7b34ce9c080552e6549ef93f1
Ratko Ćosić <ratko_cosic@hotmail.com>
2026-06-25 17:03:01 +0200

    calc

---
+++ NoBeard.Learn.AspNet.MvcApp/Test/Calculator.cs
@@ -0,0 +1,17 @@
+﻿namespace NoBeard.Learn.AspNet.MvcApp.Test;
+
+public class Calculator
+{
+    public double CalculateTax(double amount, double tax)
+    {
+        if (amount < 0)
+            throw new ArgumentException("Negative numbers are not allowed.");
+
+        return Math.Round(amount + amount * tax/100, 2); 
+    }
+
+    public double CalculateVat(double amount) 
+    {
+        return CalculateTax(amount, 25);
+    }
+}
---
+++ NoBeard.Learn.AspNet.UnitTests/CalculatorTests.cs
@@ -0,0 +1,108 @@
+﻿using NoBeard.Learn.AspNet.MvcApp.Test;
+using System.Collections;
+
+namespace NoBeard.Learn.AspNet.UnitTests;
+
+public class CalculatorTests
+{
+    [Fact]
+    public void TestCalculateVat_IntegerAmount_TaxCorrect()
+    {
+        // ARRANGE
+        var calc = new Calculator();
+        var input = 100;
+
+        // ACT
+        var result = calc.CalculateVat(input);
+
+        // ASSERT
+        Assert.IsType<double>(result);
+        Assert.NotEqual(input, result);
+        Assert.Equal(125, result);
+    }
+
+    [Fact]
+    public void TestCalculateVat_DecimalPointAmount_TaxCorrect()
+    {
+        // ARRANGE
+        var calc = new Calculator();
+
+        // ACT
+        var result = calc.CalculateVat(5.42);
+
+        // ASSERT
+        Assert.Equal(6.78, result);
+    }
+
+    [Fact]
+    public void TestCalculateVat_ZeroAmount_TaxCorrect()
+    {
+        // ARRANGE
+        var calc = new Calculator();
+
+        // ACT
+        var result = calc.CalculateVat(0);
+
+        // ASSERT
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void TestCalculateVat_NegativeAmount_ThrowsException()
+    {
+        // ARRANGE
+        var calc = new Calculator();
+
+        // ACT
+        Action result = () => calc.CalculateVat(-4.25);
+
+        // ASSERT
+        Assert.Throws<ArgumentException>(result);
+    }
+
+    [Theory]
+    [InlineData(100, 25, 125)]
+    [InlineData(200, 25, 250)]
+    [InlineData(0, 25, 0)]
+    [InlineData(100, 12, 112)]
+    [InlineData(50, 5, 52.5)]
+    public void TestCalculateTax_TheoryInlineData_TaxCorrect(double amount, double tax, double expected)
+    {
+        // ARRANGE
+        var calc = new Calculator();
+
+        // ACT
+        var result = calc.CalculateTax(amount, tax);
+
+        // ASSERT
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [ClassData(typeof(CalculatorTestData))]
+    public void TestCalculateTax_TheoryClassData_TaxCorrect(double amount, double tax, double expected)
+    {
+        // ARRANGE
+        var calc = new Calculator();
+
+        // ACT
+        var result = calc.CalculateTax(amount, tax);
+
+        // ASSERT
+        Assert.Equal(expected, result);
+    }
+
+}
+
+public class CalculatorTestData : IEnumerable<object[]>
+{
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        yield return new object[] { 100, 25, 125 };
+        yield return new object[] { 200, 25, 250 };
+        yield return new object[] { 0, 25, 0 };
+        yield return new object[] { 50, 5, 52.5 };
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
