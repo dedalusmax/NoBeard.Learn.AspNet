fb20f4e9f31e1e0173440572d659261863021b39
Ratko Ćosić <ratko_cosic@hotmail.com>
2026-07-09 19:30:53 +0200

    viewmodel

---
+++ NoBeard.Learn.AspNet.MvcApp/Models/NavLinkViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace NoBeard.Learn.AspNet.MvcApp.Models;
+
+//public record NavLinkViewModel
+//{
+//    public string Title { get; set; }
+
+//    public string ControllerName { get; set; }
+
+//    public string ActionName { get; set; }
+//}
+
+public record NavLinkViewModel(
+    string Title, 
+    string ControllerName, 
+    string ActionName);
