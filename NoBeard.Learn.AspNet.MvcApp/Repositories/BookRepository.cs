ef680816587f4926ec9d57e955199027c906090f
Ratko Ćosić <ratko_cosic@hotmail.com>
2026-07-20 19:10:01 +0200

    pole pos

--- NoBeard.Learn.AspNet.MvcApp/Repositories/BookRepository.cs
+++ NoBeard.Learn.AspNet.MvcApp/Repositories/BookRepository.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Data.SqlClient;
-using NoBeard.Learn.AspNet.MvcApp.Models;
+﻿using NoBeard.Learn.AspNet.MvcApp.Models;
 
 namespace NoBeard.Learn.AspNet.MvcApp.Repositories;
 
@@ -16,29 +15,6 @@ public class BookRepository : IBookRepository
     {
         var connectionString = _configuration.GetConnectionString("DefaultConnection");
 
-        using var connection = new SqlConnection(connectionString);
-        connection.Open();
-
-        using var command = new SqlCommand("SELECT * FROM Book", connection);
-
-        using var reader = command.ExecuteReader();
-
-        var books = new List<Book>();
-        while (reader.Read())
-        {
-            var book = new Book
-            {
-                BookId = reader.GetInt32(0),
-                Author = reader.GetInt32(1),
-                Title = reader.GetString(2),
-                Description = reader.IsDBNull(3) ? null : reader.GetString(3),
-                Genre = reader.IsDBNull(4) ? null : reader.GetString(4),
-                Stock = reader.IsDBNull(5) ? null : reader.GetInt32(5),
-                ReleaseDate = reader.GetDateTime(6)
-            };
-            books.Add(book);
-        }
-
-        return books;
+        return [];
     }
 }
