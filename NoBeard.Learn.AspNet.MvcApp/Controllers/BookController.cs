5b5b507701bd064a5a2985500dcabba03835db28
Ratko Ćosić <ratko_cosic@hotmail.com>
2026-07-20 18:30:20 +0200

    sql provider

--- NoBeard.Learn.AspNet.MvcApp/Controllers/BookController.cs
+++ NoBeard.Learn.AspNet.MvcApp/Controllers/BookController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using NoBeard.Learn.AspNet.MvcApp.Models;
 
 namespace NoBeard.Learn.AspNet.MvcApp.Controllers;
 
@@ -16,9 +18,30 @@ public class BookController : Controller
     {
         var connectionString = _configuration.GetConnectionString("DefaultConnection");
         
+        using var connection = new SqlConnection(connectionString);
+        connection.Open();
 
+        using var command = new SqlCommand("SELECT * FROM Book", connection);
 
-        return View();
+        using var reader = command.ExecuteReader();
+
+        var books = new List<Book>();
+        while (reader.Read())
+        {
+            var book = new Book
+            {
+                BookId = reader.GetInt32(0),
+                Author = reader.GetInt32(1),
+                Title = reader.GetString(2),
+                Description = reader.IsDBNull(3) ? null : reader.GetString(3),
+                Genre = reader.IsDBNull(4) ? null : reader.GetString(4),
+                Stock = reader.IsDBNull(5) ? null : reader.GetInt32(5),
+                ReleaseDate = reader.GetDateTime(6)
+            };
+            books.Add(book);
+        }
+
+        return View(books);
     }
 
     // GET: BookController/Details/5
