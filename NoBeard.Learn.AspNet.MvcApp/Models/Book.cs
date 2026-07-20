454bb5c9c563b1ff78191957ad2031514132fc94
Ratko Ćosić <ratko_cosic@hotmail.com>
2026-07-20 20:19:46 +0200

    ef core scaffold

---
+++ NoBeard.Learn.AspNet.MvcApp/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NoBeard.Learn.AspNet.MvcApp.Models;
+
+public partial class Book
+{
+    public int BookId { get; set; }
+
+    public int Author { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public string Genre { get; set; } = null!;
+
+    public int? Stock { get; set; }
+
+    public DateTime ReleaseDate { get; set; }
+
+    public virtual Author AuthorNavigation { get; set; } = null!;
+}
