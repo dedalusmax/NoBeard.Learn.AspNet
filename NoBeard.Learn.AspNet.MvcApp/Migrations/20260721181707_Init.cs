4f41311bc8d11dc37c4f79801d93f7f22bf10d59
Ratko Ćosić <ratko_cosic@hotmail.com>
2026-07-21 20:46:46 +0200

    init mig

---
+++ NoBeard.Learn.AspNet.MvcApp/Migrations/20260721181707_Init.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NoBeard.Learn.AspNet.MvcApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PetShops",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PetShops", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PetTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PetTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true),
+                    PetShopId = table.Column<int>(type: "int", nullable: false),
+                    PetTypeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pets_PetShops_PetShopId",
+                        column: x => x.PetShopId,
+                        principalTable: "PetShops",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Pets_PetTypes_PetTypeId",
+                        column: x => x.PetTypeId,
+                        principalTable: "PetTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pets_Name",
+                table: "Pets",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pets_PetShopId",
+                table: "Pets",
+                column: "PetShopId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pets_PetTypeId",
+                table: "Pets",
+                column: "PetTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PetShops_Name_Address",
+                table: "PetShops",
+                columns: new[] { "Name", "Address" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pets");
+
+            migrationBuilder.DropTable(
+                name: "PetShops");
+
+            migrationBuilder.DropTable(
+                name: "PetTypes");
+        }
+    }
+}
