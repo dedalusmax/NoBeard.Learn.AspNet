52f792eff02a6aa357d28f2b6ebc9a64c06dbce4
Ratko Ćosić <ratko_cosic@hotmail.com>
2026-07-21 19:10:21 +0200

    dbcontext

---
+++ NoBeard.Learn.AspNet.MvcApp/Data/PetShopContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NoBeard.Learn.AspNet.MvcApp.Models;
+
+namespace NoBeard.Learn.AspNet.MvcApp.Data;
+
+public class PetShopContext : DbContext
+{
+    public DbSet<PetShop> PetShops { get; set; }
+
+    public DbSet<Pet> Pets { get; set; }
+
+    public DbSet<PetType> PetTypes { get; set; }
+
+    public PetShopContext(DbContextOptions<PetShopContext> options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=PetShopDB;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Fluent API configurations can be added here if needed
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+}
--- NoBeard.Learn.AspNet.MvcApp/Models/PetShop.cs
+++ NoBeard.Learn.AspNet.MvcApp/Models/PetShop.cs
@@ -1,7 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace NoBeard.Learn.AspNet.MvcApp.Models;
 
+[Table("PetShops")]
 public class PetShop
 {
     [Key]
